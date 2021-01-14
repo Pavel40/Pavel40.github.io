0d8378df6ee2bf830e9e5b68bc4a767bd0207673
Pavel40 <35463830+Pavel40@users.noreply.github.com>
2021-01-14 22:40:50 +0100

    Remade the website completely

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
-const topButton = document.getElementById('topButton');
-window.onscroll = () => scrollFunction();
+// Scrollspy
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
 
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        topButton.style.display = 'block';
+const sectionMargin = 200;
+let lastSection = 0;
+
+window.addEventListener('scroll', () => {
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
+    if (current !== lastSection) {
+        navLinks.forEach(link => link.classList.remove('nav-link-active'));
+        lastSection = current;
+        if (navLinks[current]) {
+            navLinks[current].classList.add('nav-link-active');
+        }
+    }
+});
+
+
+// Dark theme
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+let theme;
+if (prefersDarkScheme.matches) {
+    theme = 'dark';
+}
+else if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+}
+else {
+    theme = 'light'
+}
+
+function enableDarkTheme() {
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('light-theme');
+    theme = 'dark';
+    localStorage.setItem('theme', 'dark');
+    document.getElementById('theme-icon').innerHTML = '<i class="fas fa-sun"></i>';
+}
+
+function enableLightTheme() {
+    document.body.classList.add('light-theme');
+    document.body.classList.remove('dark-theme');
+    theme = 'light';
+    localStorage.setItem('theme', 'light');
+    document.getElementById('theme-icon').innerHTML = '<i class="fas fa-moon"></i>';
+}
+
+if (theme == 'dark') {
+    enableDarkTheme();
+}
+else {
+    enableLightTheme();
+}
+
+const themeButton = document.getElementById('change-theme-button');
+themeButton.onclick = () => {
+    if (theme == 'light') {
+        enableDarkTheme();
     }
     else {
-        topButton.style.display = 'none';
+        enableLightTheme()
     }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+};
